58728dc80709c2fc9497efdd34958386c54d915d
JortzFromSchool <johnnydortiz@gmail.com>
2021-11-13 15:45:12 -0500

    ready heroku deployment

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 
 const port = process.env.PORT || 5000;
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
 
 mongoose
   .connect(db, { useNewUrlParser: true })
